cc9952bc13718f1e0ea97696d0d2a42c989419aa
Lucas Petti <luc_petti@hotmail.com>
2018-12-28 17:33:15 +0100

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 # Add countries from json file
-
+countries = JSON.parse(File.read("db/countries.json"))
+# puts countries
+array = countries.each do |country|
+  Country.create!(country) unless Country.find_by(name: country["name"])
+end
+puts array
 # Add people
