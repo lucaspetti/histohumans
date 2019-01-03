ffffef47c5273a88711add169a62780c3cedbdc9
Lucas Petti <luc_petti@hotmail.com>
2019-01-03 16:53:54 +0100

    new file to parse wikipedia for photo and bio

---
+++ db/person_data.js
@@ -0,0 +1,12 @@
+// fetch data from JSON file
+
+// const first_name =
+// const last_name =
+
+const url = `https://en.wikipedia.org/wiki/${first_name}_${last_name}`;
+
+// find data inside the url
+
+const photo = document.querySelector('.infobox.vcard img').src;
+
+const bio = document.querySelectorAll('.mw-parser-output p')[1].innerText;
