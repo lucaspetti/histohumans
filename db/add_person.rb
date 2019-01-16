668400949a1c52e7ec89813a2814139f8140d340
Lucas Petti <luc_petti@hotmail.com>
2019-01-16 22:33:00 +0100

    corrected add person code

--- db/add_person.rb
+++ db/add_person.rb
@@ -10,30 +10,32 @@ last_name = gets.chomp
 
 page = Wikipedia.find("#{first_name}_#{last_name}") || ""
 
-if page.content
-  puts "Found on Wikipedia"
-  dates = page.content[/(\W(\d{4})\W(\d{1,2})\W(\d{1,2})){2}/].split('|') || " "
-end
-
-
 puts 'Country:'
 country_name = gets.chomp
 
 puts 'Occupation:'
 occupation = gets.chomp
 
+if page.content
+  puts "Found on Wikipedia"
+  dates = page.content[/(\W(\d{4})\W(\d{1,2})\W(\d{1,2})){2}/] || ""
+  dates = dates.split('|')
+end
+
 puts 'Image URL:'
 img_url = page.main_image_url || gets.chomp
 
-puts 'Birthdate (pass as 1789-07-10):'
-birthdate = "#{dates[4]}-#{dates[5]}-#{dates[6]}" || gets.chomp
+birthdate = "#{dates[4]}-#{dates[5]}-#{dates[6]}"
+death = "#{dates[1]}-#{dates[2]}-#{dates[3]}"
 
-# Date needs to be passed as 1789-07-10
-puts 'Death: (Leave blank if person is alive)'
-death = "#{dates[1]}-#{dates[2]}-#{dates[3]}" || gets.chomp
-puts dates
-p dates
-puts death
+if dates.empty?
+  puts 'Birthdate (pass as 1789-07-10):'
+  birthdate = gets.chomp
+
+  puts 'Death: (Leave blank if person is alive)'
+  death = gets.chomp
+end
+
 death = Date.parse(death) unless death.empty?
 
 puts 'Give a short bio (can be edited later):'
