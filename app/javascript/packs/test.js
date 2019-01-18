2fb1284aa1a62878f70804db5f9cc70d5900f372
Lucas Petti <luc_petti@hotmail.com>
2019-01-18 17:02:14 +0100

    updated styling

--- app/javascript/packs/test.js
+++
@@ -1,2 +0,0 @@
-// console.log('Hello form text.js!');
-
---
+++ app/javascript/packs/toggle-country.js
@@ -0,0 +1,8 @@
+console.log('Hello form toggle country.js!');
+
+// $(document).ready(function () {
+//     $("li").click(function () {
+//         $('li > ul').not($(this).children("ul").toggle()).hide();
+
+//     });
+// });
