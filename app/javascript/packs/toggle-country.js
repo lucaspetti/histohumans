453094cf1d966e73bbf52a9f614507ca85960527
Lucas Petti <luc_petti@hotmail.com>
2019-01-18 17:39:12 +0100

    implemented toggle option in index

--- app/javascript/packs/toggle-country.js
+++ app/javascript/packs/toggle-country.js
@@ -1,4 +1,20 @@
-console.log('Hello form toggle country.js!');
+// console.log('Hello form toggle country.js!');
+
+const continents = document.querySelectorAll('.continent-link');
+
+continents.forEach(function (continent) {
+  continent.addEventListener('click', function(e) {
+    e.preventDefault();
+    var query = '.Asia';
+    var query = `.${e.target.innerText.split(' ')[0]}`;
+    console.log(query);
+    const continentCountries = document.querySelectorAll(query);
+    continentCountries.forEach(function (c) {
+      c.classList.toggle('hidden');
+    })
+    // console.log(e.target.innerText);
+  })
+})
 
 // $(document).ready(function () {
 //     $("li").click(function () {
