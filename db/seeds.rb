4b517d84326e93f916792ad756985c5311bad7cf
Lucas Petti <luc_petti@hotmail.com>
2019-01-20 13:49:29 +0100

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,10 @@ countries = JSON.parse(File.read("db/countries.json"))
 
 array = countries.each do |country|
   unless Country.find_by(name: country["name"])
+    country["continent"] = Continent.find_by(name: country["continent"]) if country["continent"]
     c = Country.create!(country)
-    c.continent = Continent.find_by(name: country["continent"]) if country["continent"]
-    c.save!
   end
 end
-
-
-
-# countries.each do |country|
-#   c = Country.find_by(name: country["name"])
-#   # puts "#{c.name} is in #{country['continent']} actual continent_id is #{c.continent_id}"
-#   if country["continent"]
-#     c.continent_id = Continent.find_by
-#     # puts "#{c.continent_id}"
-#     c.save!
-#   end
-# end
 
 puts "Countries added. Adding people..."
 
