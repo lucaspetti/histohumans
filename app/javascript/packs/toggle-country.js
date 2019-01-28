151b7e4494cbd6f3681f170b75ad9cb62e8f5e37
Lucas Petti <luc_petti@hotmail.com>
2019-01-28 16:22:19 +0100

    changed index styling

--- app/javascript/packs/toggle-country.js
+++ app/javascript/packs/toggle-country.js
@@ -1,12 +1,24 @@
 const continents = document.querySelectorAll('.continent-link');
+const countriesList = document.querySelector('.countries-list');
 
 continents.forEach(function (continent) {
   continent.addEventListener('click', function(e) {
     e.preventDefault();
+    hideCountries();
     var query = `.${e.target.innerText.split(' ')[0]}`;
     const continentCountries = document.querySelectorAll(query);
     continentCountries.forEach(function (c) {
-      c.classList.toggle('hidden');
+      c.classList.remove('hidden');
+      countriesList.appendChild(c);
     })
   })
 })
+
+function hideCountries() {
+  if (countriesList.hasChildNodes()) {
+  var children = countriesList.childNodes;
+  children.forEach(function (c) {
+    c.classList.add('hidden');
+    })
+  }
+}
