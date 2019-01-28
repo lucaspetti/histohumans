802d6b51e8266670a7fbcfdf0c7add5346b23d81
Lucas Petti <luc_petti@hotmail.com>
2019-01-29 00:56:08 +0100

    changed index styling

--- app/javascript/packs/toggle-people.js
+++ app/javascript/packs/toggle-people.js
@@ -3,8 +3,6 @@ const cardLinks = document.querySelectorAll('.card-link');
 cardLinks.forEach(function (link) {
   link.addEventListener('click', function(e) {
     e.preventDefault();
-    // e.path[1].children[3].classList.toggle('hidden');
-    console.log(e.path);
-    console.log(e.path.nextElementSibling);
+    e.path[2].children[1].classList.toggle('hidden');
   })
 })
