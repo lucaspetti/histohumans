a7f4a479c6cefd8e24d8e5b86583f882fda787d2
Lucas Petti <luc_petti@hotmail.com>
2019-02-14 10:37:59 +0100

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,23 @@ puts "Adding Continents..."
 end
 
 puts "Adding countries..."
-# countries = JSON.parse(File.read("db/countries.json"))
+countries = JSON.parse(File.read("db/countries.json"))
 
-# array = countries.each do |country|
-#   if Country.find_by(name: country["name"])
-#     c = Country.find_by(name: country["name"])
-#     c.remote_photo_url = country["photo"] unless c.photo
-#     c.save!
-#   else
-#     if country["continent"]
-#       country["continent"] = Continent.find_by(name: country["continent"])
-#       c = Country.new(country)
-#       c.flag = "https://www.countryflags.io/#{c.code}/flat/64.png"
-#       c.remote_photo_url = country["photo"]
-#       c.save!
-#     end
-#   end
-# end
+array = countries.each do |country|
+  if Country.find_by(name: country["name"])
+    c = Country.find_by(name: country["name"])
+    c.remote_photo_url = country["photo"] unless c.photo
+    c.save!
+  else
+    if country["continent"]
+      country["continent"] = Continent.find_by(name: country["continent"])
+      c = Country.new(country)
+      c.flag = "https://www.countryflags.io/#{c.code}/flat/64.png"
+      c.remote_photo_url = country["photo"]
+      c.save!
+    end
+  end
+end
 
 puts "Countries added. Adding people..."
 
