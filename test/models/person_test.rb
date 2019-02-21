3d9565188a1265379db5be19973489db48599bc7
Lucas Petti <luc_petti@hotmail.com>
2019-02-21 11:52:10 +0100

    fixed people.yml and added more tests do person model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save person without name" do
+    person = Person.new
+    assert_not person.save, "Saved the person without a name"
+  end
+
+  test "full_name returns first name and last name" do
+    person = Person.new(first_name: "John", last_name: "Lennon")
+    assert_equal "John Lennon", person.full_name, "Full name did not return correctly"
+  end
+
+  test "sample returns a person instance" do
+    sample = Person.sample
+    assert_instance_of Person, sample, "Sample is not an instance of Person"
+  end
 end
