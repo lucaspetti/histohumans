465a58d2443d1792c196c636a54024af32471482
Lucas Petti <luc_petti@hotmail.com>
2019-02-21 16:34:37 +0100

    wrote first controller test for people_controller

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get people_url
+    assert_response :success
+  end
 end
