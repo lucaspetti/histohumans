8a9007e6d7654e0f604a698855b951f66fef73b7
Lucas Petti <luc_petti@hotmail.com>
2019-02-22 16:26:01 +0100

    updated people controller test

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @person = people(:lincoln)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
   test "should get index" do
     get people_url
-
     assert_equal "index", @controller.action_name
     # assert_equal "application/x-www-form-urlencoded", @request.media_type
     assert_match "People", @response.body
+    assert_response :success
+  end
 
+  test "should show person" do
+    get person_url(@person)
     assert_response :success
   end
 end
