a473a5366ec97383074283c424c62814796648bb
Lucas Petti <luc_petti@hotmail.com>
2019-02-22 17:17:49 +0100

    writing tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+ActiveRecord::Migration.maintain_test_schema!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
