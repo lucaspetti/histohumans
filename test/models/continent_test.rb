58607c911ac876792569292346df8cb3fa91d2b9
Lucas Petti <luc_petti@hotmail.com>
2019-02-25 11:59:20 +0100

    tests written for continent model

--- test/models/continent_test.rb
+++ test/models/continent_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class ContinentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  # setup, exercise, verify, teardown
+  test "should not save continent without name" do
+    continent = Continent.new
+    assert_not continent.save, "Saved the continent without a name"
+  end
+
+  test "continent should have many countries" do
+    continent = Continent.first
+    assert_not_nil continent.countries, "countries method returns nil"
+  end
 end
