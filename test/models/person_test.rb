25ab3449073ad81349ce9283fc6ac99f60b9affd
Lucas Petti <luc_petti@hotmail.com>
2019-02-26 18:08:30 +0100

    updated tests for country, person and quiz models

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,21 +1,36 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
+  setup do
+    @person = people(:napoleon)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
   test "should not save person without name" do
     person = Person.new
     assert_not person.save, "Saved the person without a name"
   end
 
   test "full_name returns first name and last name" do
-    person = Person.new(first_name: "John", last_name: "Lennon")
-    assert_equal "John Lennon", person.full_name, "Full name did not return correctly"
+    assert_equal "Napoleon Bonaparte", @person.full_name, "Full name did not return correctly"
   end
 
   test "sample returns a person instance" do
     assert_instance_of Person, Person.sample, "Sample is not an instance of Person"
   end
 
-  test "ensures that there is a first name" do
-    assert_not_nil Person.first.first_name, "First name is nil"
+  test "should belong to a country" do
+    assert_instance_of Country, @person.country, "person does not belong to a country"
+  end
+
+  test "ensures that there is a bio" do
+    assert_not_nil Person.first.bio, "bio is nil"
+  end
+
+  test "should have a Wikipedia page" do
+    assert_instance_of Page, @person.page, "person has no Wikipedia page"
   end
 end
--- test/models/quiz_test.rb
+++ test/models/quiz_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class QuizTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @quiz = quizzes(:basicworldquiz)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
+  test "should not save quiz without name" do
+    quiz = Quiz.new(level: 'basic')
+    assert_not quiz.save, "Saved the quiz without a name"
+  end
+
+  test "should not save quiz without level" do
+    quiz = Quiz.new(name: 'First quiz')
+    assert_not quiz.save, "Saved the quiz without level"
+  end
+
+  test "quiz level should be either Basic, Intermediate or Advanced" do
+    quiz = Quiz.new(level: 'anything')
+    assert_not quiz.save, "quiz level accepted anything"
+  end
 end
