65cea86738d1098209c80bf20c9fe24459ee1371
Lucas Petti <luc_petti@hotmail.com>
2019-02-27 12:03:10 +0100

    corrected quiz model validations

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,8 @@
 class Quiz < ApplicationRecord
   belongs_to :country, optional: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+  validates :level, inclusion: { in: ['basic', 'intermediate', 'advanced'],
+                                 message: "%{value} is not a valid level" },
+                    presence: true
   has_many :questions, dependent: :destroy
 end
