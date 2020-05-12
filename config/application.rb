5f5349d3b68a2acc8f228de9ea3e850daf9d7b10
lucaspetti <luc_petti@hotmail.com>
2020-05-12 21:14:58 +0200

    Include rack-cors and cors headers

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ module Histohumans
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
