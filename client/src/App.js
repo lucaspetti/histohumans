b1c8158caa5859ee71600fa53dbee7ac32bdcff6
pettil <lucas.petti@devolute.org>
2020-08-04 12:33:53 +0200

    Include client react app

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  state = { person: {} }
+
+  componentDidMount() {
+    this.getPerson()
+  }
+
+  getPerson = async () => {
+    const request = await axios.get('http://localhost:3001/api/v1/people/id.json?sample=true')
+    const data = await request.data
+    this.setState({ person: data })
+  }
+
+  render() {
+    const { person } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Welcome to Histohumans</p>
+          <p>{person.first_name} {person.last_name}</p>
+          <a href="/">
+            <img src={person.photo} alt={person.first_name + '_' + person.last_name}  />
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
